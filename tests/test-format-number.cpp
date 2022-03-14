cd65b2f2fb6eae78eea307e87ead28d5f632dbad
BoolPurist <fnxa@posteo.de>
2022-03-14 23:47:11 +0100

    Added test for call with a few same placeholders.

--- tests/test-format-number.cpp
+++ tests/test-format-number.cpp
@@ -5,11 +5,28 @@
 #include <catch2/catch.hpp>
 #include "../Format.h"
 
-TEST_CASE( "Test case with 2 placeholders", "[Valid]" )
+TEST_CASE( "Test case with 0, 1 placeholders", "[Valid]" )
 {
   const int x{ 5 };
   const int y{ 20 };
+  const std::string expected{"(5,20)"};
 
   const std::string output = NiceGraphic::Format("({0},{1})", x, y);
-  REQUIRE(output == "(5,20)");
+  REQUIRE(output == expected);
+}
+
+TEST_CASE( "Test case with 2x0, 1, 2  placeholders", "[Valid]" )
+{
+  const std::string arrow{ ">>" };
+  const std::string face{ "*_*" };
+  const std::string smile{ ":)" };
+  const std::string expected{" >> *_* >> :) "};
+
+  const std::string output = NiceGraphic::Format(
+    " {0} {1} {0} {2} ",
+      arrow,
+      face,
+      smile
+    );
+  REQUIRE(output == expected);
 }
